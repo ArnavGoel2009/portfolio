67a0e6f3173a93e53d4256ce9faac5970b6c501f
ArnavGoel2009 <75212458+ArnavGoel2009@users.noreply.github.com>
2026-08-18 03:02:37 +0530

    feat(jarvis): export native CLI worker adapters

--- projects/jarvis-control-core/jarvis_core/__init__.py
+++ projects/jarvis-control-core/jarvis_core/__init__.py
@@ -1,3 +1,4 @@
 from .core import JarvisCore, Task, TaskStatus, EvidenceError, ClaimConflict, ApprovalRequired
 from .worker import Runner, CallableWorker, WorkerResult
 from .subprocess_worker import SubprocessWorker
+from .native_cli import NativeCLIWorker, CLIProfile, PROFILES, probe_cli
